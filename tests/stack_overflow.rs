201a2e239ab20af868b6c2b28e33a60d5f4f6182
pezfisk <marcorguezhdez@gmail.com>
2025-05-28 08:03:49 +0000

    Finished double faults

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
-use InvOS::serial_print;
+use InvOS::{QemuExitCode, exit_qemu, serial_print};
 use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -20,6 +23,32 @@ pub extern "C" fn _start() -> ! {
 fn stack_overflow() {
     stack_overflow();
     volatile::Volatile::new(0).read();
+}
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(InvOS::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_print!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
 }
 
 #[panic_handler]
